2fc9d4fbfbb293f898a68aada346ad9cdb6ab853
Fernanda <nanda.kipper@gmail.com>
2025-04-13 20:16:39 -0300

    feat: initial resolver config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '/comparer/:channelA/:channelB',
+        resolve: {
+            comparer: 'comparerResolver'
+        },
+        loadComponent: () => import('./components/comparer/comparer.component').then(m => m.ComparerComponent)
+    }
+];
