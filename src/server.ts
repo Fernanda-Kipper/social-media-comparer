f43614c1908e5a5fe82c5e4678cd8205218fffb5
Fernanda <nanda.kipper@gmail.com>
2025-04-13 19:22:04 -0300

    feat: add api endpoints

--- src/server.ts
+++ src/server.ts
@@ -8,23 +8,49 @@ import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import { listPublicVideos, getChannelDetails } from './api/youtube.client.js';
+
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
 
-/**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/**', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
+app.get('/api/search', (req, res) => {
+  const query = req.query['q'] as string;
+
+  if(!query) {
+    res.status(400).send('Missing query parameter');
+    return;
+  }
+  
+  listPublicVideos(query || '')
+    .then((videos) => {
+      res.json(videos);
+    })
+    .catch((error) => { 
+      console.log(error);
+      res.status(500).send('Error while searching');
+    });
+});
+
+app.get('/api/channel', (req, res) => {
+  const channel = req.query['id'] as string;
+
+  if(!channel) {
+    res.status(400).send('Missing channel id parameter');
+    return;
+  }
+  
+  getChannelDetails(channel)
+    .then((details) => {
+      res.json(details);
+    })
+    .catch((error) => { 
+      console.log(error);
+      res.status(500).send('Error while searching');
+    });
+});
 
 /**
  * Serve static files from /browser
