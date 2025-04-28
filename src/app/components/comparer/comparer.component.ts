6249fb9debd84f77bb5b412e32ad8b3e42a19d20
Fernanda <nanda.kipper@gmail.com>
2025-04-27 22:12:55 -0300

    feat: add page styles

--- src/app/components/comparer/comparer.component.ts
+++ src/app/components/comparer/comparer.component.ts
@@ -2,10 +2,14 @@ import { Component, inject, resource, signal } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ChannelInfo } from '../../types/youtube/types';
 import { SearchService } from '../../services/search.service';
+import { CommonModule } from '@angular/common';
+
+type SocialNetwork = 'youtube' | 'instagram';
 
 @Component({
   selector: 'app-comparer',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './comparer.component.html',
   styleUrl: './comparer.component.scss'
 })
@@ -15,6 +19,7 @@ export class ComparerComponent {
   private searchService = inject(SearchService);
 
   isComparerMode = signal(false);
+  selectedNetwork = signal<SocialNetwork>('youtube');
 
   queryA = signal('');
   queryB = signal('');
@@ -22,8 +27,11 @@ export class ComparerComponent {
   selectedChannelA = signal('');
   selectedChannelB = signal('');
 
-  channelAData: ChannelInfo | null = null;
-  channelBData: ChannelInfo | null = null;
+  showResultsA = signal(false);
+  showResultsB = signal(false);
+
+  channelAData = signal<ChannelInfo| null >(null);
+  channelBData = signal<ChannelInfo| null >(null);
 
   channelAResults = resource({
     request: () => ({q: this.queryA()}),
@@ -37,20 +45,21 @@ export class ComparerComponent {
 
   ngOnInit() {
     this.activatedRoute.data.subscribe(({data}) => {
-      if(!data) this.isComparerMode.set(false);
-      else {
+      if(!data) {
+        this.isComparerMode.set(false);
+      } else {
         this.isComparerMode.set(true);
-        this.channelAData = data.channelA;
-        this.channelBData = data.channelB;
+        this.channelAData.set(data.channelA);
+        this.channelBData.set(data.channelB);
       }
     });
   }
 
-  compare(){
+  compare() {
     this.router.navigate(['/comparer', this.selectedChannelA(), this.selectedChannelB()]);
   }
 
-  private debounce(func: (...args: any[]) => void, wait: number) {
+  private debounce(func: Function, wait: number) {
     let timeout: any;
     return (...args: any[]) => {
       clearTimeout(timeout);
@@ -59,11 +68,48 @@ export class ComparerComponent {
   }
 
   updateQueryB = this.debounce((value: string) => {
-    this.queryA.set(value);
+    this.queryB.set(value);
+    this.showResultsB.set(true);
   }, 300);
 
   updateQueryA = this.debounce((value: string) => {
     this.queryA.set(value);
+    this.showResultsA.set(true);
   }, 300);
-  
+
+  setNetwork(network: SocialNetwork) {
+    if (network === 'youtube') {
+      this.selectedNetwork.set(network);
+    }
+  }
+
+  isNetworkSelectable(network: SocialNetwork): boolean {
+    return network === 'youtube';
+  }
+
+  selectChannelA(channel: any) {
+    this.selectedChannelA.set(channel.id.channelId);
+    this.queryA.set(channel.snippet.channelTitle);
+    this.showResultsA.set(false);
+  }
+
+  selectChannelB(channel: any) {
+    this.selectedChannelB.set(channel.id.channelId);
+    this.queryB.set(channel.snippet.channelTitle);
+    this.showResultsB.set(false);
+  }
+
+  formatNumber(value: string | undefined): string {
+    if(!value) return "0"
+    const num = parseInt(value, 10);
+    if (isNaN(num)) return '0';
+    
+    if (num >= 1000000) {
+      return (num / 1000000).toFixed(1) + 'M';
+    }
+    if (num >= 1000) {
+      return (num / 1000).toFixed(1) + 'K';
+    }
+    return num.toString();
+  }
 }
